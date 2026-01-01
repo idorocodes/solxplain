3e90a5038ece35a1455430179f640358fb003b91
idorocodes <idoroyen33@gmail.com>
2026-01-01 10:09:33 +0100

    added clap and tested

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,73 @@
-use anyhow::{Ok, Result};
-use solana_client::{nonblocking::rpc_client::RpcClient, rpc_config::{CommitmentConfig, RpcAccountInfoConfig, UiAccountEncoding, UiDataSliceConfig}};
-use solana_sdk::{account::Account, pubkey::Pubkey, signature::Signature};
-use solana_transaction_status::{UiTransactionEncoding, option_serializer::OptionSerializer};
-use std::str::FromStr;
-use solana_client::rpc_request::Address;
+use clap::{Parser,Subcommand};
+mod solana;
 
 
-#[derive(Debug)]
-struct UserAccount{
-    lamorts : u64,
-    data_len : usize,
-    owner : Address,
-    executable : bool,
-    rent_epoch : u64}
-
-// #[tokio::main]
-// async fn main() -> Result<()> {
-//     let rpc_url = "https://api.devnet.solana.com";
-//     let client = RpcClient::new(rpc_url.to_string());
-
-//     let signature_str =
-//         "3qpbjjdhQ8Qd36vwLvF6u1cv9cz2FvSXtEDK4Z29AG7JDdyxy8dtKc6YeLagptX4FNZ4Xd8yh1nFfrXtECBnuQZZ";
-//     let signature = Signature::from_str(signature_str)?;
-
-//     let config = solana_client::rpc_config::RpcTransactionConfig {
-//         encoding: Some(UiTransactionEncoding::Json),
-//         commitment: Some(CommitmentConfig::confirmed()),
-//         max_supported_transaction_version: Some(0),
-//     };
-//     let tx_with_meta = client
-//         .get_transaction_with_config(&signature, config)
-//         .await?;
-
-//     println!("Tx Slot : {}", tx_with_meta.slot);
-
-//     if let Some(meta) = tx_with_meta.transaction.meta {
-//         println!("Transaction errot {:?}", meta.err);
-//         if let OptionSerializer::Some(logs) = meta.log_messages {
-//             println!(" --- Logs ----");
-//             for log in logs {
-//                 println!("{}", log)
-//             }
-//             println!("--------------")
-//         }
-//     } else {
-//         println!(
-//             "Transaction meta data not available (likely failed or not confirmed with the specified commitment)"
-//         );
-//     }
-
-//     Ok(())
-// }
-
-
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    let rpc_url = "https://api.devnet.solana.com";
-    let client = RpcClient::new(rpc_url.to_string());
-
-    let public_key = Pubkey::from_str("H3qTybhEacpa8kQGU7sxocGJuNtFsg4joKHyHcqGKdvE")?;
-   
-    let tx_with_meta = client.get_account_with_commitment(&public_key, CommitmentConfig::processed()).await?;
-    
-    
-    println!("Tx Slot : {:?}", tx_with_meta.context);
-    
-    if let Some(account) = tx_with_meta.value{
-      let user =  UserAccount{
-            lamorts : account.lamports,
-            data_len: account.data.len(),
-            owner : account.owner,
-            executable : account.executable,
-            rent_epoch : account.rent_epoch,
-        };
-        
-      println!("{:?}",user)
-    }
-   
-    
-
-    Ok(())
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct CliCommand{
+    #[command(subcommand)]
+    command : Commands,
+    #[arg(long, global = true)]
+    cluster : Option<String>
 }
+
+#[derive(Debug, Subcommand)]
+enum Commands{
+    Tx{
+        signature : String
+    },
+    Account {
+        pubkey : String
+    }
+}
+
+#[derive(Debug,Clone)]
+enum Cluster {
+    Mainnet,
+    TestNet,
+    Devnet
+}
+
+
+impl Cluster {
+    fn set_rpc (self) -> &'static str{
+        match self {
+            Cluster::Mainnet => "https://api.mainnet-beta.solana.com",
+            Cluster::Devnet => "https://api.devnet-beta.solana.com",
+            Cluster::TestNet => "https://api.testnet-beta.solana.com"
+        } 
+    }
+}
+
+fn detect_cluster (cli_cluster : Option<String>) -> Cluster{
+    
+    if let Some(cluster) = cli_cluster{
+        return match cluster.as_str() {
+            "devnet" => Cluster::Devnet,
+            "testnet" => Cluster::TestNet,
+            "mainnet" => Cluster::Mainnet,
+             _ => Cluster::Devnet
+        };
+    }
+    else {
+        Cluster::Mainnet
+    }
+}
+
+fn main () {
+    let args = CliCommand::parse();
+    
+    let rpc_url = args.cluster;
+    
+    match args.command {
+        
+        Commands::Tx { signature } => println!("Get Tx, {}",signature),
+        Commands::Account { pubkey } => println!("Get Account {}", pubkey),
+        _ => {},
+    }
+    
+    
+    
+}
+
+
---
+++ src/solana.rs
@@ -0,0 +1,188 @@
+// use anyhow::{Ok, Result};
+// use solana_client::{nonblocking::rpc_client::RpcClient, rpc_config::{CommitmentConfig, RpcAccountInfoConfig, UiAccountEncoding, UiDataSliceConfig}};
+// use solana_sdk::{account::Account, native_token::LAMPORTS_PER_SOL, pubkey::Pubkey, signature::Signature};
+// use solana_transaction_status::{UiTransactionEncoding, option_serializer::OptionSerializer};
+// use std::str::FromStr;
+// use solana_client::rpc_request::Address;
+
+
+// #[derive(Debug)]// use anyhow::{Ok, Result};
+// use solana_client::{nonblocking::rpc_client::RpcClient, rpc_config::{CommitmentConfig, RpcAccountInfoConfig, UiAccountEncoding, UiDataSliceConfig}};
+// use solana_sdk::{account::Account, native_token::LAMPORTS_PER_SOL, pubkey::Pubkey, signature::Signature};
+// use solana_transaction_status::{UiTransactionEncoding, option_serializer::OptionSerializer};
+// use std::str::FromStr;
+// use solana_client::rpc_request::Address;
+
+
+// #[derive(Debug)]
+// struct UserAccount{
+//     lamports : u64,
+//     data_len : usize,
+//     owner : Address,
+//     executable : bool,
+//     rent_epoch : u64}
+
+// // #[tokio::main]
+// // async fn main() -> Result<()> {
+// //     let rpc_url = "https://api.devnet.solana.com";
+// //     let client = RpcClient::new(rpc_url.to_string());
+
+// //     let signature_str =
+// //         "3qpbjjdhQ8Qd36vwLvF6u1cv9cz2FvSXtEDK4Z29AG7JDdyxy8dtKc6YeLagptX4FNZ4Xd8yh1nFfrXtECBnuQZZ";
+// //     let signature = Signature::from_str(signature_str)?;
+
+// //     let config = solana_client::rpc_config::RpcTransactionConfig {
+// //         encoding: Some(UiTransactionEncoding::Json),
+// //         commitment: Some(CommitmentConfig::confirmed()),
+// //         max_supported_transaction_version: Some(0),
+// //     };
+// //     let tx_with_meta = client
+// //         .get_transaction_with_config(&signature, config)
+// //         .await?;
+
+// //     println!("Tx Slot : {}", tx_with_meta.slot);
+
+// //     if let Some(meta) = tx_with_meta.transaction.meta {
+// //         println!("Transaction errot {:?}", meta.err);
+// //         if let OptionSerializer::Some(logs) = meta.log_messages {
+// //             println!(" --- Logs ----");
+// //             for log in logs {
+// //                 println!("{}", log)
+// //             }
+// //             println!("--------------")
+// //         }
+// //     } else {
+// //         println!(
+// //             "Transaction meta data not available (likely failed or not confirmed with the specified commitment)"
+// //         );
+// //     }
+
+// //     Ok(())
+// // }
+
+
+
+// #[tokio::main]
+// async fn main() -> Result<()> {
+//     let rpc_url = "https://api.devnet.solana.com";
+//     let client = RpcClient::new(rpc_url.to_string());
+
+//     let public_key = Pubkey::from_str("H3qTybhEacpa8kQGU7sxocGJuNtFsg4joKHyHcqGKdvE")?;
+   
+//     let tx_with_meta = client.get_account_with_commitment(&public_key, CommitmentConfig::processed()).await?;
+    
+    
+//     println!("Tx Slot : {:?}", tx_with_meta.context);
+    
+//     if let Some(account) = tx_with_meta.value{
+//       let user =  UserAccount{
+//             lamports : account.lamports,
+//             data_len: account.data.len(),
+//             owner : account.owner,
+//             executable : account.executable,
+//             rent_epoch : account.rent_epoch,
+//         };
+        
+//         println!("{:?}",user);
+      
+//        println!(" --- Logs ----");
+       
+//        println!("Lamports: {:?} ",user.lamports);
+//        println!("Data Len:{:?} ", user.data_len);
+//        println!("Owner: {:?}",user.owner);
+//        println!("Executable: {:?}", user.executable);
+//        println!("Rent Epoch: {:?}", user.rent_epoch)
+       
+       
+//     }
+   
+    
+
+//     Ok(())
+// }
+
+// struct UserAccount{
+//     lamports : u64,
+//     data_len : usize,
+//     owner : Address,
+//     executable : bool,
+//     rent_epoch : u64}
+
+// // #[tokio::main]
+// // async fn main() -> Result<()> {
+// //     let rpc_url = "https://api.devnet.solana.com";
+// //     let client = RpcClient::new(rpc_url.to_string());
+
+// //     let signature_str =
+// //         "3qpbjjdhQ8Qd36vwLvF6u1cv9cz2FvSXtEDK4Z29AG7JDdyxy8dtKc6YeLagptX4FNZ4Xd8yh1nFfrXtECBnuQZZ";
+// //     let signature = Signature::from_str(signature_str)?;
+
+// //     let config = solana_client::rpc_config::RpcTransactionConfig {
+// //         encoding: Some(UiTransactionEncoding::Json),
+// //         commitment: Some(CommitmentConfig::confirmed()),
+// //         max_supported_transaction_version: Some(0),
+// //     };
+// //     let tx_with_meta = client
+// //         .get_transaction_with_config(&signature, config)
+// //         .await?;
+
+// //     println!("Tx Slot : {}", tx_with_meta.slot);
+
+// //     if let Some(meta) = tx_with_meta.transaction.meta {
+// //         println!("Transaction errot {:?}", meta.err);
+// //         if let OptionSerializer::Some(logs) = meta.log_messages {
+// //             println!(" --- Logs ----");
+// //             for log in logs {
+// //                 println!("{}", log)
+// //             }
+// //             println!("--------------")
+// //         }
+// //     } else {
+// //         println!(
+// //             "Transaction meta data not available (likely failed or not confirmed with the specified commitment)"
+// //         );
+// //     }
+
+// //     Ok(())
+// // }
+
+
+
+// #[tokio::main]
+// async fn main() -> Result<()> {
+//     let rpc_url = "https://api.devnet.solana.com";
+//     let client = RpcClient::new(rpc_url.to_string());
+
+//     let public_key = Pubkey::from_str("H3qTybhEacpa8kQGU7sxocGJuNtFsg4joKHyHcqGKdvE")?;
+   
+//     let tx_with_meta = client.get_account_with_commitment(&public_key, CommitmentConfig::processed()).await?;
+    
+    
+//     println!("Tx Slot : {:?}", tx_with_meta.context);
+    
+//     if let Some(account) = tx_with_meta.value{
+//       let user =  UserAccount{
+//             lamports : account.lamports,
+//             data_len: account.data.len(),
+//             owner : account.owner,
+//             executable : account.executable,
+//             rent_epoch : account.rent_epoch,
+//         };
+        
+//         println!("{:?}",user);
+      
+//        println!(" --- Logs ----");
+       
+//        println!("Lamports: {:?} ",user.lamports);
+//        println!("Data Len:{:?} ", user.data_len);
+//        println!("Owner: {:?}",user.owner);
+//        println!("Executable: {:?}", user.executable);
+//        println!("Rent Epoch: {:?}", user.rent_epoch)
+       
+       
+//     }
+   
+    
+
+//     Ok(())
+// }
